069a731380efb4e9636a67a53670fec20ddd72e3
Rahul Razdan <RahulRazdan@fico.com>
2021-12-02 17:24:19 +0530

    network image

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
+  HttpOverrides.global= MyHttpoverrides();
   runApp(const MaterialApp(
     home: Home(),
   ));
 }
 
+class MyHttpoverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -17,13 +27,7 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.deepOrange[500],
         ),
-        body: const Center(child: Text('Hello',style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color:Colors.teal,
-            fontFamily: 'Ubuntu'
-        ),)),
+        body: const Center(child: Image(image: NetworkImage('https://images.unsplash.com/photo-1595856984276-8f6d5e979f4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'))),
         floatingActionButton: FloatingActionButton(
           child: const Text('+'),
           onPressed: (){},
